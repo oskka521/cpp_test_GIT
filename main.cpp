704116f3ce814b49f7021bcc75e6bcedca0ae6dc
Oskar Karlsson <45368410+oskka521@users.noreply.github.com>
2022-11-21 09:56:39 +0100

    Create main.cpp
    test to build

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+  for(int16_t val_u16 = 0; val_u16 < 100; val_u16++)
+  {
+    std::cout << "Hello world" << val_u16 << std::endl; 
+  }
+  return 0; 
+}
